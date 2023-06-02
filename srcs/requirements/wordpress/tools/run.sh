3e7d20f4d383d4894a48fb2d76be363a3ff374a7
inception <inception@inception.myguest.virtualbox.org>
2023-06-02 18:17:14 +0700

    fin mandatory

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 wp-cli --allow-root core download;
-# wp-cli --allow-root config create --dbname=wordpress_db --dbuser=hello --dbpass=1234 --dbhost=mariadb;
 wp-cli --allow-root config create --dbname=wordpress_db --dbuser=hello --dbpass=1234 --locale=en_DB --dbhost=mariadb;
 wp-cli --allow-root core install --url=psuanpro.42.fr --title=Hello_42 --admin_user=hello_admin --admin_password=admin_pass --admin_email=hello_admin@hotmail.com;
 service php7.3-fpm start
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-
-mkdir /run/php
-
-/usr/sbin/php-fpm7.3 -F
+chown -R www-data:www-data .
+if [ ! -d "/run/php" ]; then
+	mkdir /run/php
+fi
+/usr/sbin/php-fpm7.3 -F -R
 
