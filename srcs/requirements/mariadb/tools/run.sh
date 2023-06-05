77b9c801a0549b3d13c01853371c6ea26abc4c4a
inception <inception@inception.myguest.virtualbox.org>
2023-06-06 03:03:01 +0700

    fin mandatory

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -3,19 +3,19 @@
 service mysql restart;
 
 cat > mysql_secure_installation.sql << EOF
-ALTER USER 'root'@'localhost' IDENTIFIED BY '1234';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 FLUSH PRIVILEGES;
 EOF
-mysql -uroot -p1234 << EOF
+mysql -uroot -p$MYSQL_ROOT_PASSWORD << EOF
 CREATE DATABASE wordpress_db;
-CREATE USER 'hello'@'%' IDENTIFIED BY '1234';
-GRANT ALL PRIVILEGES ON * . * TO 'hello'@'%';
-CREATE USER 'hello2'@'%' IDENTIFIED BY '1234';
-GRANT ALL PRIVILEGES ON  wordpress_db. * TO 'hello2'@'%';
+CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON * . * TO '$MYSQL_USER'@'%';
+CREATE USER '$MYSQL_USER_DB'@'%' IDENTIFIED BY '$MYSQL_USER_DB_PASSWORD';
+GRANT ALL PRIVILEGES ON  wordpress_db. * TO '$MYSQL_USER_DB'@'%';
 FLUSH PRIVILEGES;
 EOF
 
--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 wp-cli --allow-root core download;
-wp-cli --allow-root config create --dbname=wordpress_db --dbuser=hello --dbpass=1234 --locale=en_DB --dbhost=mariadb;
-wp-cli --allow-root core install --url=psuanpro.42.fr --title=Hello_42 --admin_user=hello_admin --admin_password=admin_pass --admin_email=hello_admin@hotmail.com;
+wp-cli --allow-root config create --dbname=wordpress_db --dbuser=$MYSQL_USER_DB --dbpass=$MYSQL_USER_DB_PASSWORD --locale=en_DB --dbhost=mariadb;
+wp-cli --allow-root core install --url=$DOMAIN_NAME --title=Hello_42 --admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN@hotmail.com;
 service php7.3-fpm start
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 chown -R www-data:www-data .
